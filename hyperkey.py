d4f7a65f57cf24f34f6e01fea9d82cdda8e1f337
cartel <cartel@thoughtcrime.org.nz>
2011-07-19 08:15:29 +0000

    initial commit

---
+++ hyperkey.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+""" hyperkey - a generative approach to password management
+
+  Generate a password given:
+
+  * Seed file
+  * Service identifier (login, whatever)
+  * Passphrase 
+"""
+DEBUG = False
+from hashlib import sha256
+from getpass import getpass
+import random
+import string
+from pbkdf import pbkdf
+
+if DEBUG:
+    import pdb
+
+try:
+    import pyperclip
+    clipboard = True
+except:
+    clipboard = False
+
+
+from sys import argv
+
+# policy: (length, uppercase, numeric, symbol, secure)
+green =  ( 8, 2, 1, 0, False)
+yellow = (12, 2, 2, 1, False)
+red =    (24, 8, 3, 3, False)
+
+filename = argv[1]
+try:
+    f = open(argv[1])
+    if argv[2].lower()  in ["green", "yellow", "red"]:
+        exec "policy = %s"%argv[2].lower()
+    else:
+        print "policy defaulting to green"
+        policy = green
+    if len(argv) == 3:
+        service = getpass("service:")
+    else:
+        service = argv[3]
+except:
+    print "usage: passmaster.py seedfile policy [service]"
+    raise SystemExit
+
+passphrase = getpass("passphrase:")
+if DEBUG: pdb.set_trace()
+salt = f.read(8)
+print "hashing: ",
+s = sha256("".join(f.readlines()))
+print "done. iterating s",
+if DEBUG: pdb.set_trace()
+s.update(pbkdf(service, salt))
+print "p",
+if DEBUG: pdb.set_trace()
+s.update(pbkdf(passphrase, salt))
+print "done."
+random.seed(int(s.hexdigest(),16))
+
+spinner = "|/-\\"
+
+def generate(policy):
+    u,n,s = 0,0,0
+    length, uppercase, numeric, symbols, secure = policy
+    while (u, n, s) != (uppercase, numeric, symbols):
+        p = ""
+        u, n, s = 0,0,0 
+        for i in range(0, length):
+            if s != symbols or secure:
+                if random.randrange(0,10) == 9:
+                    p += random.choice(string.punctuation)
+                    s += 1
+                    continue
+            if u != uppercase or secure:
+                if random.randrange(0,5) == 4:
+                    p += random.choice(string.uppercase)
+                    u += 1
+                    continue
+            if n != numeric or secure:
+                if random.randrange(0,5) == 4:
+                    p += random.choice(string.digits)
+                    n += 1
+                    continue
+            p += random.choice(string.lowercase)
+    return p
+
+print generate(policy),
+
---
+++ pbkdf.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+
+"""
+Given a password p, salt s and length l, output a NIST SP800-132 compliant Master Key.
+
+Implements PBKDF2.
+original : http://matt.ucc.asn.au/src/pbkdf2.py
+
+# (c) 2004 Matt Johnston <matt @ ucc asn au>
+# This code may be freely used, distributed, relicensed, and modified for any
+# purpose.
+"""
+
+from Crypto.Hash import SHA256
+import hmac
+import binascii
+from struct import pack, unpack
+import warnings
+
+
+def prf( h, data ):
+	hm = h.copy()
+	hm.update( data )
+	return hm.digest()
+
+def xorstr( a, b ):
+	if len(a) != len(b):
+		raise "xorstr(): lengths differ"
+
+	ret = ''
+	for i in range(len(a)):
+		ret += chr(ord(a[i]) ^ ord(b[i]))
+	return ret
+
+
+def pbkdf2_F( h, salt, itercount, blocknum ):
+	U = prf( h, salt + pack('>i',blocknum ) )
+	T = U
+
+	for i in range(2, itercount+1):
+		U = prf( h, U )
+		T = xorstr( T, U )
+
+	return T
+
+def pbkdf(password, salt, itercount=10**5, keylen=32, hashfn = SHA256):
+        """ callme """
+        warnings.simplefilter("ignore", RuntimeWarning,0)
+        digest_size = hashfn.digest_size
+	# l - number of output blocks to produce
+	l = keylen / digest_size
+	if keylen % digest_size != 0:
+		l += 1
+	h = hmac.new( password, None, hashfn )
+	T = ""
+	for i in range(1, l+1):
+		T += pbkdf2_F( h, salt, itercount, i )
+	return T[0: keylen]
+
+def hexdigest(ret):
+    return "".join(map(lambda c: '%02x' % ord(c), ret))
+
+if __name__ == "__main__":
+    password  = "Those who work for the few die by the many."
+    salt = binascii.unhexlify("1234567878563412")
+    rounds = 10 ** 6
+    keylen = 32 # bytes
+    print "Iterating..."
+    ret = pbkdf( password, salt, rounds, keylen )
+    hexret = "".join(map(lambda c: '%02x' % ord(c), ret))
+    print "key: %s "%hexret
