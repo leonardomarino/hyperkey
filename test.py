bde5bfa56aa476281cb9fbd845672324d79a9d22
Leonardo Marino-Ramirez <marino@marino-johnson.org>
2025-11-29 17:57:50 -0500

    Update test.py
    Updated test result.

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ p = main(args, clipboard_enabled=False)
 
 try:
     # This is the expected output string based on your original file's contents
-    expected = "Jp5IcAmgTV9Nh%md2lo2_oD!sn32/Bzr"
+    expected = "4zRfjGU"v*apsxO+r-83SKnI16c9ravY"
     assert p == expected
     print("\n[+] Test succeeded! Output matches expected deterministic password.")
 except AssertionError:
